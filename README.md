cff2752518fe24fabe564d73208fd8880a37922f
xlord <h.rashidi128@gmail.com>
2021-04-12 16:37:03 +0430

    init repo

---
+++ README.md
@@ -0,0 +1,32 @@
+=== endpay ===
+Contributors: siturra
+Donate link: https://endpay.cl
+Tags: endpay, axios, webpay, nodejs
+Requires at least: 5.1
+Tested up to: 5.6.2
+Requires PHP: 7.2
+Stable tag: 1.0.0
+License: MIT
+License URI: https://github.com/HRashidi/webpay-bahamta/blob/main/LICENSE
+ 
+Recibe pagos en Chile mediante endpay
+
+== Description ==
+Permite el uso de endpay en WooCommerce, endpay opera sobre transbank y permite recibir pagos mediante tarjetas de crédito & débito mediante webpay en Chile. 
+
+== Installation ==
+Para instalar este plugin de forma manual debes seguir los siguientes pasos:
+
+1. Descomprime el plugin a la carpeta `/wp-content/plugins/`
+2. Activa el plugin en el menú 'Plugins' en Wordpress
+3. Configura el nuevo medio de pago en  WooCommerce -> Settings -> Payment Gateways
+
+== Changelog == 
+= 1.0.2 = 
+* se agrega modo producción.
+
+= 1.0.1 = 
+* Se implementa webhook para actualizar ordenes internas de woocommerce.
+
+= 1.0.0 = 
+* Versión inicial estable.
---
+++ index.js
@@ -0,0 +1,115 @@
+"use strict";
+const axios = require('axios').default;
+const pjson = require('./package.json');
+
+const BASE_URL = 'https://webpay.bahamta.com/api';
+const DOCS_URL = 'https://webpay.bahamta.com/doc/api';
+const USER_AGENT = `Webpay NodeJS Library - ${pjson.version}`;
+
+axios.interceptors.response.use(
+	response => {
+		return response;
+	},
+	error => {
+		if(!error.response)
+			error.response = {
+				data: { ok: false, error: 'NETWORK_ERROR', apidoc: DOCS_URL},
+				status: 500,
+			};
+		return Promise.reject(error.response);
+	}
+);
+
+const serverCall = config => {
+	config.headers = {
+		"user-agent": USER_AGENT,
+	};
+
+	config.baseURL = BASE_URL;
+	config.timeout = 60 * .5 * 1000; // Let's say you want to wait at least .5 mins
+	return axios(config);
+};
+
+/**
+	* @param {string} api_key - The bahamta api-key: 'webpay:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx:zzzzzzzz-zzzz-zzzz-zzzz-zzzzzzzzzzzz'
+	* @param {string} reference - The order number (should be unique): 'order#1004'
+	* @param {number} amount_irr - The amount of money in Rials: 100000
+	* @param {string} callback_url - Call back url for bahamta recall (should start with http or https) : 'https://mydomain.ir/checkout/order#1004'
+	* @param {string} payer_mobile - The payer mobile number: '98912*******'
+	* @param {string} trusted_pan - The trusted credit cards number list: '6219234531241234,623254******5645,5453612376879876'
+
+	* @returns {Promise} Promise object represents server response success=({ok: true, result}), failure = ({ok: false, error})
+*/
+const payment = async (
+	api_key,
+	reference,
+	amount_irr,
+	callback_url,
+	payer_mobile = null,
+	trusted_pan = null
+	) => {
+
+	try {
+
+		let params = {
+			api_key,
+			reference,
+			amount_irr,
+			callback_url,
+			payer_mobile,
+			trusted_pan
+		};
+		const response = await serverCall({method: 'GET', url:`/create_request`, params: params});
+		return response.data;
+	} catch (error) {
+		return {...response.data, apidoc: DOCS_URL};
+	}
+}
+
+/**
+	* @param {string} api_key - The bahamta api-key: 'webpay:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx:zzzzzzzz-zzzz-zzzz-zzzz-zzzzzzzzzzzz'
+	* @param {string} reference - The order number (should be unique): 'order#1004'
+	* @param {number} amount_irr - The amount of money in Rials: 100000
+	* @returns {Promise} Promise object represents server response success=({ok: true, result}), failure = ({ok: false, error})
+*/
+const verify = async (
+	api_key,
+	reference,
+	amount_irr
+	) => {
+		
+	try {
+		let params = {
+			api_key,
+			reference,
+			amount_irr
+		};
+		const response = await serverCall({method: 'GET', url:`/confirm_payment`, params});
+		return response.data;
+	} catch (error) {
+		return {...response.data, apidoc: DOCS_URL};
+	}
+}
+
+module.exports = {
+    payment : payment,
+	verify : verify
+};
+
+
+// Test
+let api_key = 'webpay:f8bdfa41-f2e3-4899-a6c5-ef8de7ef0a01:ee46fc5e-05cb-4986-aa35-4b6580be289b';
+let reference = '35300510481';
+let amount_irr = 100000 * 10;
+let callback_url = 'https://xlord.net/chechout/success';
+let payer_mobile = '09171793871';
+
+let paymentTest = async () => {
+	let res = await payment(api_key, reference, amount_irr, callback_url, payer_mobile);
+	console.log(res);
+
+	res = await verify(api_key, reference, amount_irr);
+	console.log(res);
+}
+
+paymentTest()
